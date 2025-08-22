3ff46d049851e50c6adde114cab8242580ed737d
eliotjorge <eliotjorge@hotmail.com>
2025-08-22 20:24:07 +0200

    Update swcache.js

--- assets/js/data/swcache.js
+++ assets/js/data/swcache.js
@@ -12,7 +12,7 @@ const resource = [
   '{{ "/sw.js" | relative_url }}',
 
   /* --- HTML --- */
-  '{{ "/index.html" | relative_url }}',
+  '{{ "/" | relative_url }}',
   '{{ "/404.html" | relative_url }}',
 
   {% for tab in site.tabs %}
